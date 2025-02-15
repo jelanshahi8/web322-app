53bef52145f629f844ec5f22c0c7eb7db3272c66
jelanshahi8 <shahijelan@gmail.com>
2025-02-14 19:45:42 -0500

    Update store-service.js

--- store-service.js
+++ store-service.js
@@ -2,18 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 let items = [];
-let categories = {};
+let categories = [];
 
 function initialize() {
     return new Promise((resolve, reject) => {
-        fs.readFile(path.join(__dirname, './data/items.json'), 'utf8', (err, data) => {
+        fs.readFile(path.join(__dirname, 'data/items.json'), 'utf8', (err, data) => {
             if (err) {
                 reject("Unable to read items.json");
                 return;
             }
             items = JSON.parse(data);
 
-            fs.readFile(path.join(__dirname, './data/categories.json'), 'utf8', (err, data) => {
+            fs.readFile(path.join(__dirname, 'data/categories.json'), 'utf8', (err, data) => {
                 if (err) {
                     reject("Unable to read categories.json");
                     return;
